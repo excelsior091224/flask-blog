0813318cb986ba50272d9e57fce54afece58322a
excelsior091224 <excelsior091224@gmail.com>
2020-03-13 13:44:08 +0900

    2nd commit

--- blog/views/index_view.py
+++ blog/views/index_view.py
@@ -12,9 +12,9 @@ def register():
     title = '新規登録'
     if request.method == 'POST':
         if request.form['name'] == '':
-            print('名前を入力してください')
+            flash('名前を入力してください')
         elif request.form['password'] == '':
-            print('パスワードを入力してください')
+            flash('パスワードを入力してください')
         else:
             account = Account(
                 name=request.form['name'],
@@ -23,6 +23,8 @@ def register():
             db.session.add(account)
             db.session.commit()
             session['logged_in'] = True
+            session['name'] = request.form['name']
+            flash('ユーザー名:' + session['name'] + 'で登録しました')
             return redirect(url_for('index'))
     return render_template('register.html', title=title)
 
@@ -31,9 +33,9 @@ def login():
     title = 'ログイン'
     if request.method == 'POST':
         if request.form['name'] == '':
-            print('名前を入力してください')
+            flash('名前を入力してください')
         elif request.form['password'] == '':
-            print('パスワードを入力してください')
+            flash('パスワードを入力してください')
         else:
             user_name = request.form['name']
             user = Account.query.filter_by(name=user_name).first()
@@ -42,9 +44,12 @@ def login():
             else:
                 password = request.form['password']
                 if password != user.hashed_password:
+                    flash('パスワードが違います')
                     return redirect(url_for('login'))
                 else:
                     session['logged_in'] = True
+                    session['name'] = request.form['name']
+                    flash('ユーザー名:' + session['name'] + 'でログインしました')
                     return redirect(url_for('index'))
     return render_template('login.html', title=title)
 
